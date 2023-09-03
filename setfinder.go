7eb891b548cd32463b9a772484b2be60e1762632
frankwhoee <huifrank@live.com>
2023-09-03 15:39:13 -0700

    initial commit

---
+++ setfinder.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	// open file
+	pwd, _ := os.Getwd()
+	f, err := os.Open(pwd + "/fiveletterset.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file at the end of the program
+	defer f.Close()
+
+	// read the file line by line using scanner
+	scanner := bufio.NewScanner(f)
+	all_words := make([]string, 0)
+	for scanner.Scan() {
+		// do something with a line
+		all_words = append(all_words, scanner.Text())
+	}
+	fmt.Println(len(all_words))
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	start := time.Now()
+	output := get_distinct_words(all_words, 5)
+	fmt.Println(output)
+	end := time.Now()
+	fmt.Println(end.Sub(start))
+}
+
+func string_to_set(s string) map[int32]bool {
+	output := make(map[int32]bool)
+	for _, letter := range s {
+		output[letter] = true
+	}
+	return output
+}
+
+func word_shares_letters(word string, letters map[int32]bool) bool {
+	for _, ch := range word {
+		if _, exists := letters[ch]; exists {
+			return true
+		}
+	}
+	return false
+}
+
+func get_distinct_words(allowable_words []string, depth int) []string {
+	// while the current picked word is less than len of allowable word
+	for i := 0; i < len(allowable_words); i++ {
+		i_discard := i + 1
+		// pick the first word that is not discarded
+		current_word := allowable_words[i]
+		// move all words that share letters to the left
+		current_letters := string_to_set(current_word)
+		for j := i + 1; j < len(allowable_words); j++ {
+			if word_shares_letters(allowable_words[j], current_letters) {
+				allowable_words[j], allowable_words[i_discard] = allowable_words[i_discard], allowable_words[j]
+				i_discard++
+			}
+		}
+		// if there are still words to the right,
+		if i_discard <= len(allowable_words) {
+			// end if we've reached sufficient depth
+			if depth == 1 {
+				output := make([]string, 0)
+				return append(output, current_word)
+			} else {
+				// pass slice with pointer starting where words are allowed, recursively
+				output := get_distinct_words(allowable_words[i_discard:], depth-1)
+				// If the output is not None, break and return the result with chosen word added to picked_words
+				if output != nil {
+					return append(output, current_word)
+				}
+			}
+		}
+	}
+	return nil
+}
---
+++ wordfilter.py
@@ -0,0 +1,106 @@
+def file_exists(file_path):
+    try:
+        with open(file_path, "r") as f:
+            return True
+    except FileNotFoundError:
+        return False
+
+if not file_exists("fiveletterset.txt"):
+    filename = "words_alpha.txt"
+    # load filename as a list
+    words = []
+    with open(filename, "r") as f:
+        words.extend(f.read().split("\n"))
+    fiveletterwords = []
+    for word in words:
+        if len(word) == 5 and len(set(word)) == 5:
+            fiveletterwords.append(word)
+
+    # write the list to a file
+    with open("fiveletterset.txt", "w") as f:
+        f.write("\n".join(fiveletterwords))
+else:
+    with open("fiveletterset.txt", "r") as f:
+        fiveletterwords = f.read().split("\n")
+
+# Old python implementation that doesn't work
+
+# letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+#            "w", "x", "y", "z"}
+# used = set()
+# word_list = []
+#
+# def register_word(word, used):
+#     for letter in word:
+#         if letter in used:
+#             raise Exception(f"word {word} contains duplicate letter {letter}")
+#         used.add(letter)
+#
+# def deregister_word(word, used):
+#     for letter in word:
+#         used.remove(letter)
+#
+# def swap(arr, a,b):
+#     temp = arr[a]
+#     arr[a] = arr[b]
+#     arr[b] = temp
+#
+# def depthprint(depth, *args):
+#     print("\t" * depth, *args, sep=" ", end="\n")
+#
+# def get_letter_distinct_word(used_letters, allowable_words, current_word_list, depth_limit=5, depth=0):
+#     if depth_limit == len(word_list):
+#         depthprint(depth,"Depth limit reached")
+#         return current_word_list
+#     temp_allowable_words = allowable_words.copy()
+#     iteration_num = 0
+#     discard_ptr = len(temp_allowable_words)
+#     current_word = ""
+#     while discard_ptr >= len(temp_allowable_words) - 1 and iteration_num < len(temp_allowable_words):
+#         current_word = temp_allowable_words[iteration_num]
+#         current_word_set = set(current_word)
+#         discard_ptr = iteration_num + 1
+#         for i in range(discard_ptr, len(temp_allowable_words)):
+#             word = temp_allowable_words[i]
+#             for letter in word:
+#                 if letter in current_word_set:
+#                     swap(temp_allowable_words, i, discard_ptr)
+#                     discard_ptr += 1
+#                     break
+#         iteration_num += 1
+#     if iteration_num >= len(temp_allowable_words) or discard_ptr >= len(temp_allowable_words):
+#         return None
+#     else:
+#         temp_allowable_words = temp_allowable_words[discard_ptr:]
+#         add_word(current_word, current_word_list, used_letters)
+#         depthprint(depth, current_word_list)
+#         depthprint(depth, f"words left: {len(temp_allowable_words)}")
+#         output = get_letter_distinct_word(used_letters, temp_allowable_words, current_word_list, depth_limit, depth + 1)
+#         while output is None and len(temp_allowable_words) > 0:
+#             depthprint(depth,f"No solution given {current_word}. Current word list: {current_word_list}")
+#             discard_words, current_word_list = current_word_list[depth:], current_word_list[:depth]
+#             for word in discard_words:
+#                 print(f"removing {word}")
+#                 deregister_word(word, used_letters)
+#             current_word = temp_allowable_words[0]
+#             add_word(current_word, current_word_list, used_letters)
+#             temp_allowable_words = temp_allowable_words[1:]
+#             output = get_letter_distinct_word(used_letters, temp_allowable_words, current_word_list, depth_limit, depth)
+#         if len(temp_allowable_words) == 0:
+#             return None
+#         depthprint(depth,"Found a word")
+#         depthprint(depth, current_word_list)
+#         return get_letter_distinct_word(used_letters, temp_allowable_words, current_word_list, depth_limit, depth + 1)
+#
+#
+# def remove_word(current_word, current_word_list, used_letters):
+#     depthprint(0, f"removing {current_word}. Current word list: {current_word_list}")
+#     current_word_list.remove(current_word)
+#     deregister_word(current_word, used_letters)
+#
+#
+# def add_word(current_word, current_word_list, used_letters):
+#     depthprint(0, f"adding {current_word}. Current word list: {current_word_list}")
+#     current_word_list.append(current_word)
+#     register_word(current_word, used_letters)
+
